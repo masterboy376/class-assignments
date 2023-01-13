160109d75a766786bb644269a3c2e763f30ef39c
Sambhav Kaushik <92575005+masterboy376@users.noreply.github.com>
2023-01-13 19:35:09 +0530

    Update assignment_4.c

--- assignment_4.c
+++ assignment_4.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 
 const double e = 2.7182;
 
-// problem 1 
+// problem 1
 void prob1_1(int n) {
   for (int i = 1; i <= n; i++) {
     for (int j = 1; j <= 2 * n - 1; j++) {
@@ -64,11 +64,11 @@ void prob1_5(int n) {
     printf("\n");
   }
   for (int j = 1; j <= 2 * n - 1; j++) {
-      if (n > abs(n - j)) {
-        printf("%d ", n - abs(j - n));
-      }
+    if (n > abs(n - j)) {
+      printf("%d ", n - abs(j - n));
     }
-    printf("\n");
+  }
+  printf("\n");
   for (int i = n - 1; i >= 1; i--) {
     for (int j = 1; j <= 2 * n - 1; j++) {
       if (i > abs(n - j)) {
@@ -84,7 +84,7 @@ void prob1_6(int n) {
   for (int i = 1; i <= n; i++) {
     for (int j = 1; j <= 2 * n - 1; j++) {
       if (i > abs(n - j)) {
-        printf("%d ", cnt*cnt);
+        printf("%d ", cnt * cnt);
         cnt++;
       } else {
         printf(" ");
@@ -94,143 +94,136 @@ void prob1_6(int n) {
   }
 }
 
-void Print_BinCoeff(int x)
-{
-    int temp=x;
-    int answer=x;
-    int c=2;
-    for(int i=0;i<=x;i++)
-    {	
-        if(i==0 || i==x)
-        {
-            printf("%-4d",1);
-        }
-        else
-        {
-            printf("%-4d",answer);
-            answer=answer*(temp-1);
-            answer=answer/c;
-            temp--;
-            c++;
-        }
+void Print_BinCoeff(int x) {
+  int temp = x;
+  int answer = x;
+  int c = 2;
+  for (int i = 0; i <= x; i++) {
+    if (i == 0 || i == x) {
+      printf("%-4d", 1);
+    } else {
+      printf("%-4d", answer);
+      answer = answer * (temp - 1);
+      answer = answer / c;
+      temp--;
+      c++;
     }
-    printf("\n");
+  }
+  printf("\n");
 }
-void prob1_7(int n){
-  for(int i=0;i<n;i++)
-    {
-        for (int k = n -  i-1 ; k > 0; k--)
-        {
-            printf("  ");
-        }
-        Print_BinCoeff(i);
+void prob1_7(int n) {
+  for (int i = 0; i < n; i++) {
+    for (int k = n - i - 1; k > 0; k--) {
+      printf("  ");
     }
+    Print_BinCoeff(i);
+  }
 }
 
 // problem 2
-void prob2_1(){
-    int n;
-    printf("Kindly Enter input(s) below\n");
-    scanf("%d", &n);
-    float ans = 0;
-    for(int i = 1 ; i<n+1;i++){
-        ans += 1.0/i;
-    }
-    printf("ANSWER: %.2f\n", ans);
+void prob2_1() {
+  int n;
+  printf("Kindly Enter input(s) below\n");
+  scanf("%d", &n);
+  float ans = 0;
+  for (int i = 1; i < n + 1; i++) {
+    ans += 1.0 / i;
+  }
+  printf("ANSWER: %.2f\n", ans);
 }
 
-void prob2_2(){
-    int n;
-    printf("Kindly Enter input(s) below\n");
-    scanf("%d", &n);
-    float ans = 0;
-    for(int i = 1 ; i<n+1;i++){
-        if(!(i&1) || i==1)
-            ans+=1.0/i;
-        else    
-            ans-=1.0/i;
-    }
-    printf("ANSWER: %.2f\n", ans);
+void prob2_2() {
+  int n;
+  printf("Kindly Enter input(s) below\n");
+  scanf("%d", &n);
+  float ans = 0;
+  for (int i = 1; i < n + 1; i++) {
+    if (!(i & 1) || i == 1)
+      ans += 1.0 / i;
+    else
+      ans -= 1.0 / i;
+  }
+  printf("ANSWER: %.2f\n", ans);
 }
 
-void prob2_3(){
-    int n;
-    printf("Kindly Enter input(s) below\n");
-    scanf("%d", &n);
-    float ans = 0;
-    for(int i = 1 ; i<n+1;i++){
-        ans += 1.0/(i*i);
-    }
-    printf("ANSWER: %.2f\n", ans);
+void prob2_3() {
+  int n;
+  printf("Kindly Enter input(s) below\n");
+  scanf("%d", &n);
+  float ans = 0;
+  for (int i = 1; i < n + 1; i++) {
+    ans += 1.0 / (i * i);
+  }
+  printf("ANSWER: %.2f\n", ans);
 }
 
-void prob2_4(){
-    int n;
-    printf("Kindly Enter input(s) below\n");
-    scanf("%d", &n);
-    int dp[n];
-    dp[0] = 1;
-    for(int  i = 1 ; i<n+1; i++){
-        dp[i] = i*dp[i-1];
-    }
-    float ans = 0;
-    for(int i = n ; i>=1;i--){
-        ans += 1.0/dp[i];
-    }
-    printf("ANSWER: %.2f\n", ans);
+void prob2_4() {
+  int n;
+  printf("Kindly Enter input(s) below\n");
+  scanf("%d", &n);
+  float dp[n+1];
+  dp[0] = 1.0;
+  for (int i = 1; i < n + 1; i++) {
+    dp[i] = i * dp[i - 1];
+  }
+  float ans = 0;
+  for (int i = n; i >= 1; i--) {
+    ans += 1 / dp[i-1];
+    
+  }
+  printf("ANSWER: %.2f\n", ans);
 }
 
 // problem 3
-void prob3_1(){
-    int n;
-    float x;
-    printf("Kindly Enter input(s) below\n");
-    scanf("%f %d", &x, &n);
+void prob3_1() {
+  int n;
+  float x;
+  printf("Kindly Enter input(s) below\n");
+  scanf("%f %d", &x, &n);
 
-    int dp[n];
-    dp[0] = 1;
-    for(int  i = 1 ; i<n+1; i++){
-        dp[i] = i*dp[i-1];
-    }
-    float ans = 0 ;
-    for(int i = 0 ; i< n+1; i++){
-        ans+=pow(x,i)/dp[i];
-    }
-    printf("ANSWER: %.2f\n", ans);
+  int dp[n];
+  dp[0] = 1;
+  for (int i = 1; i < n + 1; i++) {
+    dp[i] = i * dp[i - 1];
+  }
+  double ans = 0;
+  for (int i = 0; i < n + 1; i++) {
+    ans += pow(x, i) / dp[i];
+  }
+  printf("ANSWER: %.2f\n", ans);
 }
 
-void prob3_2(){
-    int n;
-    float x, ans, t;
-    printf("Kindly Enter input(s) below\n");
-    scanf("%f %d", &x, &n);
+void prob3_2() {
+  int n;
+  float x, ans, t;
+  printf("Kindly Enter input(s) below\n");
+  scanf("%f %d", &x, &n);
 
-    x=x*3.14159/180;
-    t=x;
-    ans = x;
-    for(int i = 1 ; i< n+1; i++){
-        t = (t*(-1)*x*x)/(2*i*(2*i+1));
-        ans = ans+t;
-    }
-    printf("ANSWER: %.2f\n", ans);
+  x = x * 3.14159 / 180;
+  t = x;
+  ans = x;
+  for (int i = 1; i < n + 1; i++) {
+    t = (t * (-1) * x * x) / (2 * i * (2 * i + 1));
+    ans = ans + t;
+  }
+  printf("ANSWER: %.2f\n", ans);
 }
 
-void prob3_3(){
-    int n;
-    float x, ans=1, t=1;
-    printf("Kindly Enter input(s) below\n");
-    scanf("%f %d", &x, &n);
+void prob3_3() {
+  int n;
+  float x, ans = 1, t = 1;
+  printf("Kindly Enter input(s) below\n");
+  scanf("%f %d", &x, &n);
 
-    x=x*3.14159/180;
-    for(int i = 1 ; i< n+1; i++){
-        t = (t*(-1)*x*x)/(2*i*(2*i-1));
-        ans = ans+t;
-    }
-    printf("ANSWER: %.2f\n", ans);
+  x = x * 3.14159 / 180;
+  for (int i = 1; i < n + 1; i++) {
+    t = (t * (-1) * x * x) / (2 * i * (2 * i - 1));
+    ans = ans + t;
+  }
+  printf("ANSWER: %.2f\n", ans);
 }
 
-int main()
-{
-    prob3_3();
-    return 0;
+int main() {
+  prob1_6(5);
+  return 0;
 }
